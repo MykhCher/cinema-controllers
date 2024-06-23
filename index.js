daa74ebf497d262ccd17ef34a96c5e04f2f59965
MykhCher <micher1240@gmail.com>
2024-06-23 16:34:09 +0300

    serve static, add style for homepage

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const http = require('http');
+const express = require('express');
+const path = require('path');
 // =====
 const app = require('./src/app');
 
@@ -6,6 +8,8 @@ const app = require('./src/app');
 const PORT = 5000;
 const HOST_NAME = '127.0.0.1';
 const IS_SECURE = false;
+
+app.use(express.static(path.resolve('public')))
 
 const server = http.createServer(app);
 
